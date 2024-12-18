d02c6321c7cdbbcd8cad1f0bf913ed700b7b812d
haris-afr <haris.afridi1312@gmail.com>
2024-12-18 20:16:22 +0500

    setup vigenere

--- vigenere.cpp
+++ vigenere.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-	
+string vigenereEncryptor(string message, string key){
+	string encrypted = "";
+}
+
+string vigenereDecryptor(string message, string key){
+	string decrypted = "";
 }
