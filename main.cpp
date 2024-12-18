5b88cd9ac4c1e5bed285e3a05f7f8de86581fc86
shoaib hussain <shoaibhussainiqbal135@gmail.com>
2024-12-19 00:24:37 +0500

    Update main.cpp
    added xor file

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 const char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 #include "vigenere.cpp"
 #include "caesar.cpp"
+#include "XOREncryptorDecryptor.cpp"
 using namespace std;
 
 
