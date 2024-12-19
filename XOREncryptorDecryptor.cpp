41b65ae226ebedbe39415d31800600eabfb4b8e1
shoaib hussain <shoaibhussainiqbal135@gmail.com>
2024-12-19 09:59:27 +0500

    Update XOREncryptorDecryptor.cpp
    fixed return

--- XOREncryptorDecryptor.cpp
+++ XOREncryptorDecryptor.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 string XOREncryptorDecryptor(string message){
-    string encrypt;
+    string encrypt, encrypted = "";
     char XORkey = 'T';
     int length = message.length();
-    cout<<"here";
     for(int i=0; i<length; i++){
         encrypt[i]= message[i] ^ XORkey;
-        cout<<encrypt[i];
+        encrypted.push_back(encrypt[i]);
+
     }
 
-    return encrypt;
+    return encrypted;
 
 }
