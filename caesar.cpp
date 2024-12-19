8b3f141b531b7e83f9beb033a448ab234552cafa
haris-afr <haris.afridi1312@gmail.com>
2024-12-19 10:57:57 +0500

    comments

--- caesar.cpp
+++ caesar.cpp
@@ -6,16 +6,14 @@ using namespace std;
 string caesarEncryptor(string message){
 	string encrypted = "";
 	for (int messageIndex = 0; messageIndex < message.size(); messageIndex++){
-		char current_letter = 'a';
 		int current_letter_index = 0;
 		for (int j = 0; j < 26; j++){ //find the current index of the message letter in the alphabet array
 			if (message[messageIndex] == alphabet[j]){
-				current_letter = alphabet[j];
 				current_letter_index = j;
 			}
 		}
 		
-		int encrypted_letter_index = current_letter_index - 3;
+		int encrypted_letter_index = current_letter_index - 3; //move back 3 spaces
 		if (encrypted_letter_index < 0){
 			encrypted_letter_index += 26;
 		}
@@ -29,16 +27,15 @@ string caesarDecryptor(string message){
 	string decrypted = "";
 	
 	for (int messageIndex = 0; messageIndex < message.size(); messageIndex++){
-		char current_letter = 'a';
 		int current_letter_index = 0;
-		for (int j = 0; j < 26; j++){ //find the current index of the message letter in the alphabet array
+		for (int j = 0; j < 26; j++){
 			if (message[messageIndex] == alphabet[j]){
 				current_letter = alphabet[j];
 				current_letter_index = j;
 			}
 		}
 		
-		int decrypted_letter_index = current_letter_index + 3;
+		int decrypted_letter_index = current_letter_index + 3; //move forward 3 spaces
 		if (decrypted_letter_index > 25){
 			decrypted_letter_index -= 26;
 		}
--- vigenere.cpp
+++ vigenere.cpp
@@ -17,14 +17,14 @@ string vigenereEncryptor(string message, string key){
 		int key_letter_index = 0;
 		
 		for (int j = 0; j < 26; j++){ //use same process to find the current index of the key letter in the alphabet array
-			if (key[messageIndex % key.size()] == alphabet[j]){
+			if (key[messageIndex % key.size()] == alphabet[j]){//in order to repeat key we use modulo operator
 				key_letter_index = j;
 			}
 		}
 		
-		int encrypted_letter_index = current_letter_index + key_letter_index;
+		int encrypted_letter_index = current_letter_index + key_letter_index; 
 		if (encrypted_letter_index > 25){
-			encrypted_letter_index -= 26;
+			encrypted_letter_index -= 26; //whenever alphabet index overflows we reset it
 		}
 		encrypted.push_back(alphabet[encrypted_letter_index]);
 	}
@@ -33,12 +33,12 @@ string vigenereEncryptor(string message, string key){
 }
 
 string vigenereDecryptor(string message, string key){
-	string decrypted = "";
+	string decrypted = ""; //same logic is used in decryptor but reversed
 	
 	for (int messageIndex = 0; messageIndex < message.size(); messageIndex++){
 		char current_letter = 'a';
 		int current_letter_index = 0;
-		for (int j = 0; j < 26; j++){ //find the current index of the message letter in the alphabet array
+		for (int j = 0; j < 26; j++){
 			if (message[messageIndex] == alphabet[j]){
 				current_letter = alphabet[j];
 				current_letter_index = j;
@@ -46,7 +46,7 @@ string vigenereDecryptor(string message, string key){
 		}
 		int key_letter_index = 0;
 		
-		for (int j = 0; j < 26; j++){ //use same process to find the current index of the key letter in the alphabet array
+		for (int j = 0; j < 26; j++){
 			if (key[messageIndex % key.size()] == alphabet[j]){
 				key_letter_index = j;
 			}
